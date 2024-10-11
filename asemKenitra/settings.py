6ec92ef0d5891ef48de556c9b69b2a9e79c3aaf2
chagaibou <chagaiboumahamadoumaiga@gmail.com>
2024-10-11 17:56:58 +0100

    modification de la variable denv

--- asemKenitra/settings.py
+++ asemKenitra/settings.py
@@ -15,18 +15,9 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env(env_file=str(BASE_DIR / "asemKenitra" / ".venv"))
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG")
-
-
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+SECRET_KEY = "django-insecure-k@%c8^#e*9)5vv_iid^#*2_)(z79(sy%axc(%8w3r(=5f=hc2x"
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
